e1884e988d7a6ff700c1bc5d6b53c92bd77c183d
Phil Nguyen <nquangphuong@gmail.com>
2022-02-25 18:00:10 +0700

    Add Flutter app

--- app/src/app/ng-attributes.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NgAttributesDirective } from './ng-attributes.directive';
-
-describe('NgAttributesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgAttributesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
