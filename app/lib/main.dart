52f359c7dbdfe658ec74c4fd0ad2f5e18de4bb12
Phil Nguyen <nquangphuong@gmail.com>
2022-03-03 13:34:26 +0700

    Use descriptor api

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,19 +1,14 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-import 'package:xml2json/xml2json.dart';
-
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:js' as js;
 
 const String _baseUrl = 'http://localhost:8080';
-const String _crafterApiGetContent = '/studio/api/1/services/api/1/content/get-content.json';
+const String _craftercmsApiDescriptor = '/api/1/site/content_store/descriptor.json';
 const String _crafterSite = 'flutter-sample';
-const String _authorization = 'Bearer eyJhbGciOiJQQkVTMi1IUzUxMitBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiY3R5IjoiSldUIiwicDJjIjo4MTkyLCJwMnMiOiJvZDhFcGZ5VHNTeHVZVHNEIn0.D1duKec1uFngkhohTKG5O1s7YMQtAcmQGTIKyCriGiNk7o3bXhE6LnAHZqmb_fa6_NN2DwPdWXA7A0l7CPdIsw1R-Khl2fJQ.5L6-ZLV0rdGPiHA_avl_lA.RU2nodWyL0fldnHhdR92Y97IjWclqDq0rXfs_CKAbm7PACEHf6SDAFG3HTA9ACZF8rh1AwBiRAIMLOtFyxgdiuh4Y8FOFPN55wuFqoZ_EvnPi2FGBeI2oJUbYkK91n25yIckrfcEXEB7uQqqSkckoAFgaqO_hHrgeI_OV3yV59SckYIqh3LU1avYZd6zrZcOhHebZlEZH7wI_WONyWIkX6YGIEl-f1jYvTnzEiY3mVXmx1bm_AJGWhjzHX4AbgxarZilM6njK-2a0loKjxuKXuCF2vqHNC0mEn3F_LJd5i7wlB5nveXLiMetufyIJkLbu6hVG6zKnhOm4Lk1_MRzhg.u6eEMh2oJs2DhY9mfdfChP7Eh34mzDmyGrdNqs57kcA';
 
 void main() {
   runApp(const MaterialApp(
@@ -83,44 +78,50 @@ class Model {
   final String path;
   final String navLabel;
   final String title;
-  final Hero hero;
+  final List<Hero> heros;
 
   const Model({
     required this.id,
     required this.path,
     required this.navLabel,
     required this.title,
-    required this.hero,
+    required this.heros,
   });
 
-  factory Model.fromJson(String path, Map<String, dynamic> json, Hero hero) {
+  factory Model.fromJson(String path, Map<String, dynamic> json) {
+    List<Hero> heros = [];
+
+    if(json['page']['content_o']['item'] != null) {
+      heros = (json['page']['content_o']['item'] as List)
+        .map((dynamic item) => Hero.fromJson(item))
+        .toList();
+    }
     return Model(
       id: json['page']['objectId'],
       path: path,
       navLabel: json['page']['navLabel'],
       title: json['page']['title_s'],
-      hero: hero,
+      heros: heros,
     );
   }
 }
 
 class Hero {
+  final String id;
   final String backgroundImg;
   final String title;
 
   const Hero({
+    required this.id,
     required this.backgroundImg,
     required this.title,
   });
 
   factory Hero.fromJson(Map<String, dynamic> json) {
-    final transformer = Xml2Json();
-    transformer.parse(json['content']);
-    final content = transformer.toParker();
-    final parsedJson = jsonDecode(content);
     return Hero(
-      backgroundImg: _baseUrl + parsedJson['component']['background_s'],
-      title: parsedJson['component']['title_s'],
+      id: json['key'],
+      backgroundImg: _baseUrl + json['component']['background_s'],
+      title: json['component']['title_s'],
     );
   }
 }
@@ -148,63 +149,30 @@ class AboutModel {
   }
 }
 
-Future<Hero> fetchHero(String path) async {
-  final String url = _baseUrl + _crafterApiGetContent + '?site_id=' + _crafterSite + '&path=' + path;
-  final response = await http.get(
-    Uri.parse(url),
-    headers: {
-      HttpHeaders.authorizationHeader: _authorization,
-    },
-  );
-
-  if (response.statusCode == 200) {
-    return Hero.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load model');
-  }
-}
-
 Future<Model> fetchModel(path) async {
-  final String url = _baseUrl + _crafterApiGetContent + '?site_id=' + _crafterSite + '&path=' + path;
+  final String url = _baseUrl + _craftercmsApiDescriptor + '?site_id=' + _crafterSite + '&flatten=true&url=' + path;
   final response = await http.get(
-    Uri.parse(url),
-    headers: {
-      HttpHeaders.authorizationHeader: _authorization,
-    },
+    Uri.parse(url)
   );
 
   if (response.statusCode == 200) {
     final json = jsonDecode(response.body);
-    final transformer = Xml2Json();
-    transformer.parse(json['content']);
-    final content = transformer.toParker();
-    final parsedJson = jsonDecode(content);
 
-    final heroPath = parsedJson['page']['content_o']['item']['include'];
-    final hero = await fetchHero(heroPath);
-
-    return Model.fromJson(path, parsedJson, hero);
+    return Model.fromJson(path, json);
   } else {
     throw Exception('Failed to load model');
   }
 }
 
 Future<AboutModel> fetchAboutModel(path) async {
-  final String url = _baseUrl + _crafterApiGetContent + '?site_id=' + _crafterSite + '&path=' + path;
+  final String url = _baseUrl + _craftercmsApiDescriptor + '?crafterSite=' + _crafterSite + '&flatten=true&url=' + path;
   final response = await http.get(
-    Uri.parse(url),
-    headers: {
-      HttpHeaders.authorizationHeader: _authorization,
-    },
+    Uri.parse(url)
   );
 
   if (response.statusCode == 200) {
     final json = jsonDecode(response.body);
-    final transformer = Xml2Json();
-    transformer.parse(json['content']);
-    final content = transformer.toParker();
-    final parsedJson = jsonDecode(content);
-    return AboutModel.fromJson(path, parsedJson);
+    return AboutModel.fromJson(path, json);
   } else {
     throw Exception('Failed to load model');
   }
@@ -243,35 +211,43 @@ class _HomePageState extends State<HomePage> {
                         snapshot.data!.title,
                         style: Theme.of(context).textTheme.headline4
                       ),
-                      Stack(
-                        children: [
-                          Image.network(
-                            snapshot.data!.hero.backgroundImg,
-                            height: 300,
-                            fit: BoxFit.fitWidth,
-                          ),
-                          Positioned(
-                            bottom: 100,
-                            left: 0,
-                            right: 0,
-                            child: Container(
-                              height: 100,
-                              decoration: BoxDecoration(
-                                color: Colors.black.withOpacity(0.2),
+                      ListView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: snapshot.data!.heros.map((hero) {
+                          return Stack(
+                            children: [
+                              Image.network(
+                                hero.backgroundImg,
+                                height: 300,
+                                width: double.infinity,
+                                alignment: Alignment.center,
+                                fit: BoxFit.cover,
                               ),
-                              child: Center(
-                                child: Text(
-                                  snapshot.data!.hero.title,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 24,
+                              Positioned(
+                                bottom: 100,
+                                left: 0,
+                                right: 0,
+                                child: Container(
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black.withOpacity(0.2),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      hero.title,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ),
-                        ],
+                            ],
+                          );
+                        }).toList(),
                       ),
                     ],
                   );
