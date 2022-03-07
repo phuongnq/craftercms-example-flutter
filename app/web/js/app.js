8e23068808c3b9d16bf6bd81239c80d7cab38632
Phil Nguyen <nquangphuong@gmail.com>
2022-03-07 14:47:07 +0700

    Update XB init

--- app/web/js/app.js
+++ app/web/js/app.js
@@ -1,16 +1,22 @@
+let callback;
+
 function initICE(path, id, label) {
   let elm = document.querySelector('flt-glass-pane');
-  if (elm) {
-    const attributes = {
-      'data-craftercms-model-id': id,
-      'data-craftercms-model-path': path,
-      'data-craftercms-label': label
-    };
-    for (let i = 0; i < Object.keys(attributes).length; i++) {
-      const key = Object.keys(attributes)[i];
-      const value = attributes[key];
-      elm.setAttribute(key, value);
-    }
-    craftercms.xb.initInContextEditing({ path: attributes['data-craftercms-model-path'] });
+  if (!elm) {
+    return;
   }
+  const attributes = {
+    'data-craftercms-model-id': id,
+    'data-craftercms-model-path': path,
+    'data-craftercms-label': label
+  };
+  for (let i = 0; i < Object.keys(attributes).length; i++) {
+    const key = Object.keys(attributes)[i];
+    const value = attributes[key];
+    elm.setAttribute(key, value);
+  }
+  if (callback) {
+    callback.unmount();
+  }
+  callback = craftercms?.xb?.initExperienceBuilder({ path });
 }
